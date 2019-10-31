ef036328946ada0badb31199a5e472e567c48dab
Pankaj Shrestha <pankajshrestha@gmail.com>
2019-10-31 10:58:34 -0500

    Additional test

--- pqueue.py
+++ pqueue.py
@@ -15,15 +15,26 @@ class PriQueue:
 
 
 
-def test_priqueue():
+def test_priqueue(items, remove_item):
+    #print(items)
     q = PriQueue()
-    q.push(4)
-    q.push(3)
-    q.push(7)
-    q.push(10)
-    q.remove(4)
-    assert [ q.pop() for _ in range(3) ] == [3, 7, 10]
+    for item in items:
+        q.push(item)
+    
+    # Remove the given item
+    q.remove(remove_item)
+    items.remove(remove_item)
+
+    #for _ in range(len(items)):
+    #    print(q.pop())
+    
+    # Verify that items come out in the proper order
+    assert [ q.pop() for _ in range(len(items))] == sorted(items)
+    #assert [ q.pop() for _ in range(len(items))] == (items)
 
 if __name__ == '__main__':
-    test_priqueue()
+    test_priqueue([4,3,7,10], 4)
+    test_priqueue([9,2,1,8,5],2)
+    test_priqueue([4,1,6],1)
+
 
