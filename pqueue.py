16c18ceec5ff34a1601c8b7990a923bde92baef2
Pankaj Shrestha <pankajshrestha@gmail.com>
2019-10-31 10:34:08 -0500

    test moved to function

--- pqueue.py
+++ pqueue.py
@@ -14,12 +14,16 @@ class PriQueue:
         self.heap.remove(item)
 
 
-q = PriQueue()
-q.push(4)
-q.push(3)
-q.push(7)
-q.push(10)
-q.remove(4)
 
+def test_priqueue():
+    q = PriQueue()
+    q.push(4)
+    q.push(3)
+    q.push(7)
+    q.push(10)
+    q.remove(4)
+    assert [ q.pop() for _ in range(3) ] == [3, 7, 10]
 
-assert [ q.pop() for _ in range(3) ] == [3, 7, 10]
+if __name__ == '__main__':
+    test_priqueue()
+
