15a363b54bea0e614f2016f3bb5f3bdd0cbf785f
Pankaj Shrestha <pankajshrestha@gmail.com>
2019-10-31 11:22:53 -0500

    with Hypothesis testing. Did not understand the bug and its solution

--- pqueue.py
+++ pqueue.py
@@ -1,4 +1,6 @@
 import heapq
+from hypothesis import given
+from hypothesis.strategies import lists, integers
 
 class PriQueue:
     def __init__(self):
@@ -12,11 +14,15 @@ class PriQueue:
 
     def remove(self, item):
         self.heap.remove(item)
+        heapq.heapify(self.heap) ## Did not undestand why there is a bug, visually the list are matching, not sure how this line addresses the bug
 
 
-
+@given(lists(integers(min_value=0, max_value=9),
+            unique=True, min_size=10, max_size=10),
+            integers(min_value=0, max_value=9))
 def test_priqueue(items, remove_item):
     #print(items)
+    print('Trying with:', items, remove_item)
     q = PriQueue()
     for item in items:
         q.push(item)
@@ -28,13 +34,15 @@ def test_priqueue(items, remove_item):
     #for _ in range(len(items)):
     #    print(q.pop())
     
+    pop_list = [q.pop() for _ in range(len(items))]
     # Verify that items come out in the proper order
-    assert [ q.pop() for _ in range(len(items))] == sorted(items)
-    #assert [ q.pop() for _ in range(len(items))] == (items)
+    print('Assert Visual Test:', pop_list, sorted(items))
+    #assert [ q.pop() for _ in range(len(items)) ] == sorted(items)
+    assert pop_list == sorted(items)
+    
 
 if __name__ == '__main__':
-    test_priqueue([4,3,7,10], 4)
-    test_priqueue([9,2,1,8,5],2)
-    test_priqueue([4,1,6],1)
+    test_priqueue()
+    
 
 
